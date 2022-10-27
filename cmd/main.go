e30756bcb6bb00f839fffc153be155a31d57baec
michalmorus <michal_morus@discovery.com>
2022-10-27 16:36:25 +0200

    init commit

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"runtime"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		var m runtime.MemStats
+		runtime.ReadMemStats(&m)
+
+		c.JSON(http.StatusOK, gin.H{
+			"name":       "server1",
+			"alloc":      bToMb(m.Alloc),
+			"totalAlloc": bToMb(m.TotalAlloc),
+			"sys":        bToMb(m.Sys),
+			"numGC":      m.NumGC,
+		})
+	})
+
+	r.Run()
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
